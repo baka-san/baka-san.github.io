5db83b66cd9f57adbc53a95c42441a3c27045a91
Grant <gsbackes@gmail.com>
2018-01-14 23:31:28 +0900

    Work on styling and BlocJams post.

---
+++ _includes/code/blocjams/album-refactored.js
@@ -0,0 +1,380 @@
+// Store state of playing songs
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
+
+$(function(){ 
+
+  var createSongRow = function(songNumber, songName, songLength) {
+    var template =
+       '<tr class="album-view-song-item">'
+     + '  <td class="song-item-number" data-song-number="' + songNumber
+     + '">' + songNumber + '</td>'
+     + '  <td class="song-item-title">' + songName + '</td>'
+     + '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
+     + '</tr>';
+
+    var $row = $(template);
+
+    var clickHandler =  function() {
+
+      var songNumber = parseInt($(this).attr('data-song-number'));
+
+      // No song is playing, start playing new
+      if (currentlyPlayingSongNumber === null) {
+
+        // Set the initial volume
+        setVolume(currentVolume);
+
+        // Set the seek bar position to current
+        var volumeRatio = currentVolume/100;
+        updateSeekPercentage($('.player-bar .volume'), volumeRatio);
+
+        // Play the song!
+        playSong($(this));
+      } 
+
+      // Play or pause the current song
+      else if (currentlyPlayingSongNumber === songNumber) {
+
+        // Current song is paused, play it
+        if (currentSoundFile.isPaused()) {
+          playSong($(this));
+        } 
+        // Current song is playing, pause it
+        else {
+          pauseSong($(this));
+        }
+      } 
+
+      // Change to a new song
+      else if (currentlyPlayingSongNumber !== songNumber) {
+        // change play/pause button back to song number
+          var currentSongElement = getSongNumberCell(currentlyPlayingSongNumber);
+          currentSongElement.html(currentSongElement.attr('data-song-number'));
+
+          // Stop the current song
+          currentSoundFile.stop();
+
+          // Start playing a new song
+          playSong($(this));
+      } 
+    };
+
+    var playSong = function(clicked) {
+
+      // Change the pause/play button to pause
+      clicked.html(pauseButtonTemplate);
+
+      // Update song variable info if new song
+      if (currentlyPlayingSongNumber !== songNumber) {
+        // Update song variable info
+        setSong(songNumber);
+      }
+
+      // Set the audio file to play
+      currentSoundFile.play();
+
+      // update player bar song name
+      updatePlayerBarSong();
+
+      // change player bar play button to pause
+      $('.play-pause > span').attr('class', 'ion-pause')
+
+      // Make the seek bar stay updated as song plays
+      updateSeekBarWhileSongPlays();
+    };
+
+    var pauseSong = function(clicked) {
+      
+      // change the pause/play button to play
+      clicked.html(playButtonTemplate);
+
+      // change player bar play button to play
+      $('.play-pause > span').attr('class', 'ion-play');
+
+      // Set the audio file to pause
+      currentSoundFile.pause();
+    };
+
+    var onHover = function(event) {
+      var songItem = $(this).find('.song-item-number');
+      var songNumber = parseInt(songItem.attr('data-song-number'));
+
+      // Display play button on mouseover/mouseleave IF NOT playing
+      if (songNumber !== currentlyPlayingSongNumber) {
+
+        // Change to play button on mouseover
+        songItem.html(playButtonTemplate);
+      }
+    };
+
+    var offHover = function(event) {
+      var songItem = $(this).find('.song-item-number');
+      var songNumber = parseInt(songItem.attr('data-song-number'));
+
+      if (songNumber !== currentlyPlayingSongNumber) {
+        songItem.html(songNumber);
+      }
+    };
+
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    return $row;
+  };
+
+  var setSong = function(songNumber) {
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+      formats: ['mp3', ''],
+      preload: true
+    });
+    setVolume(currentVolume);
+  };
+
+  var seek = function(time) {
+    if (currentSoundFile) {
+        currentSoundFile.setTime(time);
+    }
+  }
+
+  var setVolume = function(volume) {
+    if(currentSoundFile) {
+      currentSoundFile.setVolume(volume);
+    }
+
+    // If user changes the volume before a song is selected
+    else {
+      currentVolume = volume;
+    }
+  };
+
+  var getSongNumberCell = function(number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+  };
+
+  var setCurrentAlbum = function(album) {
+    currentAlbum = album;
+    $albumTitle.text(album.title);
+
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+
+    $albumSongList.empty();
+
+    for (var i = 0; i < album.songs.length; i++) {
+      $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+      $albumSongList.append($newRow);
+    }
+  };
+
+  var updateSeekBarWhileSongPlays = function() {
+    if (currentSoundFile) {
+        
+      currentSoundFile.bind('timeupdate', function(event) {
+        // Current time in player
+        var currentTime = this.getTime();
+        var seekBarFillRatio = currentTime / this.getDuration();
+        var $seekBar = $('.seek-control .seek-bar');
+
+        updateSeekPercentage($seekBar, seekBarFillRatio);
+
+        // Update current time in the player bar
+        setCurrentTimeInPlayerBar(currentTime);
+      });
+    }
+  };
+
+  var setCurrentTimeInPlayerBar = function(currentTime) {
+    currentTime = filterTimeCode(currentTime);
+    $('.player-bar .current-time').text(currentTime);
+  };
+
+  var setTotalTimeInPlayerBar = function(totalTime) {
+    totalTime = filterTimeCode(totalTime);
+    $('.player-bar .total-time').text(totalTime);
+  };
+  
+  var filterTimeCode = function(timeInSeconds) {
+    var displayHours = Math.floor(timeInSeconds / 3600)
+    var displayMin = Math.floor(timeInSeconds / 60) - displayHours*60;
+    var displaySec = Math.floor(timeInSeconds) - (displayMin*60) - (displayHours*3600);
+
+    displayHours = (displayHours > 0) ? displayHours + ':' : '';
+    displayMin = (displayMin >= 10) ? displayMin : '0' + displayMin; 
+    displaySec = (displaySec >= 10) ? displaySec : '0' + displaySec;
+
+    return displayHours + displayMin + ':' + displaySec;
+  };
+
+  var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+    
+    // Make sure the volume is between 0-100
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+  };
+
+  var setupSeekBars = function() {
+    // Find both volume / audio control seekbars
+    var $seekBars = $('.player-bar .seek-bar');
+  
+    // Move the thumb when the bar is clicked
+    $seekBars.click(function(event) {
+      // event.pageX = offset of click from page left
+      // $(this).offset().left = offset of bar from page left
+      // offsetX = amount the playbar moved
+      // barWidth = total width of playbar
+      var offsetX = event.pageX - $(this).offset().left;
+      var barWidth = $(this).width();
+      
+      // Find ratio seekbar changed
+      var seekBarFillRatio = offsetX / barWidth;
+      
+      // Change the currentSoundFile's position
+      if ($(this).parent().attr('class') == 'seek-control') {
+        //seek(seekBarFillRatio * currentSongFromAlbum.duration);
+        seek(seekBarFillRatio * currentSoundFile.getDuration());
+      }
+      else {
+        setVolume(seekBarFillRatio * 100);
+      }
+
+      // Actually update the position of the thumb
+      updateSeekPercentage($(this), seekBarFillRatio);
+    });
+
+    // Allow user to drag the thumb 
+    $seekBars.find('.thumb').mousedown(function(event) {
+      // Find the seekbar
+      var $seekBar = $(this).parent();
+  
+      // Make thumb move on drag
+      $(document).bind('mousemove.thumb', function(event){
+        // event.pageX = offset of click from page left
+        // $(this).offset().left = offset of bar from page left
+        // offsetX = amount the playbar moved
+        // barWidth = total width of playbar
+        var offsetX = event.pageX - $seekBar.offset().left;
+        var barWidth = $seekBar.width();
+        
+        // Find ratio seekbar changed
+        var seekBarFillRatio = offsetX / barWidth;
+      
+        // Change the currentSoundFile's position
+        if ($seekBar.parent().attr('class') == 'seek-control') {
+          //seek(seekBarFillRatio * currentSongFromAlbum.duration);
+          seek(seekBarFillRatio * currentSoundFile.getDuration());
+        }
+        else {
+          setVolume(seekBarFillRatio);
+        }
+
+        // Actually update the position of the thumb
+        updateSeekPercentage($seekBar, seekBarFillRatio);
+      });
+  
+      // Make thumb stop moving when unclicked
+      $(document).bind('mouseup.thumb', function() {
+          $(document).unbind('mousemove.thumb');
+          $(document).unbind('mouseup.thumb');
+      });
+    });
+  };
+
+  var updatePlayerBarSong = function() {
+    // update player bar song name
+    $('.player-bar .artist-name').text(currentAlbum.artist);
+    $('.player-bar .song-name').text(currentSongFromAlbum.title);
+    $('.player-bar .artist-song-mobile').text(currentSongFromAlbum.title + ' - ' + currentAlbum.artist);
+    
+    // .getDuration() is firing before currentSoundFile is set,
+    // though it works during debugger...
+    //var totalTime = currentSoundFile.getDuration();
+
+    var totalTime = currentSongFromAlbum.duration;
+    setTotalTimeInPlayerBar(totalTime);
+  };
+
+  var trackIndex = function(album, song) {
+    return album.songs.indexOf(song);
+  };
+
+  // Next button: go to next song
+  var nextPrevSong = function() {
+
+    // Check if a song is playing
+    if (currentSoundFile) {
+
+      // Find current song's index
+      var incSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+
+      // Check if it's the next button
+      if ($(this).hasClass('next')) {
+        incSongIndex++;
+
+        // Check if it's the last song
+        if (incSongIndex >= currentAlbum.songs.length) {
+          incSongIndex = 0;
+        }
+      }
+
+      // Else it's the previous button
+      else {
+        incSongIndex--;
+
+        // Check if it's the first song
+        if (incSongIndex < 0) {
+          incSongIndex = currentAlbum.songs.length - 1;
+        }
+      }
+
+      // Find the next/prev song's element
+      var incSongElement = getSongNumberCell(incSongIndex + 1);
+
+      // Simulate a click on the selected element in the song table
+      incSongElement.trigger('click');
+    }
+  };
+
+  var togglePlayFromPlayerBar = function() {
+
+    // There is a currently playing song
+    if (currentSoundFile) {
+      // Find the currentlyPlayingSongElement
+      var songElement = getSongNumberCell(currentlyPlayingSongNumber);
+    }
+
+    // No currentlyPlayingSong, play first song
+    else {
+      // Find the first song in the 
+      var songElement = getSongNumberCell(1);
+    }
+
+    // Simulate a click on the selected element in the song table
+    songElement.trigger('click');
+  };
+
+  // Album button templates
+  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+  
+  // Set random album -- testing
+  setCurrentAlbum(albumPicasso);
+
+  setupSeekBars();
+
+  // Set next button listener
+  $('.main-controls .next').click(nextPrevSong);
+  $('.main-controls .previous').click(nextPrevSong);
+  $('.main-controls .play-pause').click(togglePlayFromPlayerBar);
+});
+
--- _portfolio/blocjams.md
+++ _portfolio/blocjams.md
@@ -6,44 +6,39 @@ thumbnail-path: "img/blocjams/blocjams.gif"
 short-description: BlocJams is a Spotify-esque music streaming service, allowing users to listen to all of their favorite albums at the click of a button.
 ---
 
+{% include figure-center.html url="/img/blocjams/blocjams.png" title="" class="" %}
 
-{:.center}
-![]({{ site.baseurl }}/img/blocjams/blocjams.png)
 
 ## Overview
-BlocJams was the first project of Bloc's [Web Development Program](https://www.bloc.io/web-developer-track/syllabus?mkt_tok=eyJpIjoiTjJVeVpXUmlaVGs0WmpFeCIsInQiOiJlVmtWQlRcL202STlXQUVSaVpmWnUrTEo0amJXMmhEZ1wvTE5LYUtmVHBycUY4blhrKzZtMWlTNGZJa2pocDAwb2xsMWE1cUU5eTFxWDZES3pJQWFqS0p1SXMzYTcxcnJic0ZNZnpaSGRVNDhpNExjaUZuYms1ZDloMXZ6OUxzQ1hzIn0%3D). Before starting this project, I spent a good month studying basic JavaScript - variables, arrays, loops, functions, conditionals, and everything else. BlocJams gave me the perfect opportunity to put all of this knowledge to use. BlocJams is a Spotify-esque music streaming service, allowing users to listen to all of their favorite albums at the click of a button. The code was initially written with JavaScript, which was then rewritten in jQuery. After that, I scrapped all of my precious code again and rewrote the site using Angular. This post will discuss the site page by page, first focusing on the jQuery version of the site and then discussing the Angular version after. The JavaScript source code can be found [here](https://github.com/baka-san/bloc-jams/tree/checkpoint-15-html-css), though conceptually the code is virtually the same, so it will not be included in this post. 
+BlocJams was the first project of Bloc's [Web Development Program](https://www.bloc.io/web-developer-track/syllabus?mkt_tok=eyJpIjoiTjJVeVpXUmlaVGs0WmpFeCIsInQiOiJlVmtWQlRcL202STlXQUVSaVpmWnUrTEo0amJXMmhEZ1wvTE5LYUtmVHBycUY4blhrKzZtMWlTNGZJa2pocDAwb2xsMWE1cUU5eTFxWDZES3pJQWFqS0p1SXMzYTcxcnJic0ZNZnpaSGRVNDhpNExjaUZuYms1ZDloMXZ6OUxzQ1hzIn0%3D). Before starting this project, I spent a good month studying basic JavaScript - variables, arrays, loops, functions, conditionals, and everything else. BlocJams gave me the perfect opportunity to put all of this knowledge to use. BlocJams is a Spotify-esque music streaming service, allowing users to listen to all of their favorite albums at the click of a button. The code was initially written with JavaScript, and then rewritten in jQuery. After that, all of my precious code was scrapped again and rewritten using Angular. This post will discuss the site page by page, first focusing on the jQuery version of the site and then discussing the Angular version after. The JavaScript source code can be found [here](https://github.com/baka-san/bloc-jams/tree/checkpoint-15-html-css), though conceptually the code is virtually the same, so it will not be included in this post. Also, in order to avoid clutter, non-essential code is {% 
+include modal.html link-text="viewable via modals " id="1" type="code" title="path/to/code/hello_world.rb" language="ruby" 
+body='puts "Hello World!"'
+%} and will be highlighted as such.
 
 ## JavaScript and jQuery
 
-### Landing Page
+#### *Landing Page*
 
-{% include figure-left.html url="/img/blocjams/landing-scroll-event.jpg" title="Fig 2: " class="" %}
+{% include figure-left.html url="/img/blocjams/landing-scroll-event.jpg" title="Fig 1: Triggering Animations" class="" %}
 
+{% include figure-left.html url="/img/blocjams/landing-animation.gif" title="Fig 2: The Animation" class="landing-animation" %}
 
-{% include figure-left.html url="/img/blocjams/landing-animation.gif" title="Fig 1: Animated Content" class="" %}
-
-
-{% include modal.html id="1" type="code" link-text="link 1" title="app/services/subscriptions_service.rb" language="ruby" file="code/not-wikipedia/subscriptions_service.rb" %}
-
-{% include modal.html id="2" type="code" link-text="link 2" title="app/services/subscriptions_service.rb" language="ruby" 
-body=""
-%}
+Users are greeted by BlocJams' stylish landing page. As seen in Figure 1, there are three elements which come into view as the user scrolls down the page. This effect was achieved using css transforms and transitions {% include modal.html link-text="transforms and transitions" id="2" type="code" title="styles/landing.css" language="css" file="code/blocjams/landing-transforms-transitions.css" %}. The elements are initially not visible due to the opacity being set to zero. The window is watched and when the user has scrolled far enough down the page (see Figure 2), a jQuery function called {% include modal.html link-text="animatePoints " id="3" type="code" title="scripts/landing.js" language="javascript" file="code/blocjams/landing-jquery.js" %}  is called, changing the opacity to 1 and animating the elements.
 
 
-Users are greeted by BlocJams' stylish landing page. As seen in Figure 1, there are three elements which come into view as the user scrolls down the page. This effect was achieved using css transforms and transitions[??]. The elements are initially not visible due to the opacity being set to zero. The window is watched and when the user has scrolled far enough down the page (see Figure 2), a jQuery function called animatePoints[??] is called, changing the opacity to 1 and animating the elements.
+#### *Music Library*
+{% include figure-left.html url="/img/blocjams/music-library.png" title="Fig 3: Music Library" class="" %}
 
-
-### Music Library
-{% include figure-left.html url="/img/blocjams/.png" title="Fig 3: " class="" %}
-
-The music library isn't anything special in it's current form. The idea is that that users would see their music and be able to filter by options such as artist, album, and song title. There would also be an option to upload or sync music. As this project is focused more on the aspect of playing music and using jQuery to animate the playbar, this aspect was left out. Instead, a default album called *The Colors* is tiled repeatedly using jQuery to fill up the library.
+The music library isn't anything special in it's current form. The idea is that that users would see their music and be able to filter by options such as artist, album, and song title. There would also be an option to upload or sync music. As this project was focused on playing music and using jQuery to animate the playbar, this aspect was left out. Instead, a default album called *The Colors* is tiled repeatedly using jQuery to fill up the library.
 
 
 ## Album View
 
-So a user clicks a song and it plays...sounds pretty simple, right? WRONG! There is so much jQuery magic going on behind the scenes to make song play and the view update correctly. The album view is by far the coolest part of BlocJams. Let's overview some of the features:
+So a user clicks a song and it plays...sounds pretty simple, right? WRONG! There is so much jQuery magic going on behind the scenes to make a song play and the view update correctly. The album page is by far the coolest part of BlocJams.{% include modal.html link-text="The javascript can be viewed here" id="4" type="code" title="scripts/album-refactored.js" language="javascript" file="code/blocjams/album-refactored.js" %}, but let's explore what's happening step by step:
 
-- When the page loads, setCurrentAlbum[??] sets the page specific html using the album object provided, which is defined in `fixtures.js`.
+- When the page loads, setCurrentAlbum[??] sets the the applies the correct album information to the page. The album information is stored in 
+
+page specific html using the album object provided, which is defined in `fixtures.js`.
 
 - 
 
@@ -76,13 +71,3 @@ So a user clicks a song and it plays...sounds pretty simple, right? WRONG! There
 Give an example of changing javascript to jquery.
 
 Being that BlocJams was the first project in the curriculum and that some students have never studied code before, there was a lot of hand holding going on. I didn't really like this, so I usually looked at the goal of a checkpoint, wrote the code, and then checked with instruction after. Thus, my JavaScript code ended up being quite different than the source code. I found this style of learning to be more challenging, but overall more useful. 
-
-
-
-## Explanation
-In the era of music streaming, there are a multitude of streaming services to choose from: Spotify, Pandora, Apple Music, SoundCloud, Google Play Music, and so on. While there are many benefits to streaming music, some people may not have the streaming capabilities or just want to enjoy listening to their own music library. Enter BlocJams. 
-
-## Problem
-BlocJams was designed with simplicity in mind. Within minutes, the user can download the app and begin listening to their favorite music -- no login required. 
-
-
